ac8aab1b419f1382b3e4ac92f6e2fb31c3975f46
Prabir <Prabir@LAPTOP-6MJUL34T>
2021-02-13 12:54:37 -0800

    Simplified Heirarchy

--- survive2020/src/survive2020/Main.java
+++ survive2020/src/survive2020/Main.java
@@ -4,6 +4,16 @@ import javax.swing.*;
 
 public class Main {
   public static void main(String[] args) {
-    Frame myFrame = new Frame();
+    JFrame frame = new JFrame();
+    
+    frame.setTitle("Survive 2020");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400, 500);
+    frame.setResizable(false);
+    frame.setLayout(null);
+    frame.setVisible(true);
+    ImageIcon img = new ImageIcon("calendar.png");
+    frame.setIconImage(img.getImage());
+    /*this.getContentPane().setBackground(new Color(126, 200, 80));*/
   }
 }
